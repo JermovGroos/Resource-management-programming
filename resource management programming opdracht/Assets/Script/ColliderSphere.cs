f3a5bbaed78439bf5bb9afeedac9b5f20b449176
JermovGroos <33859010+JermovGroos@users.noreply.github.com>
2019-06-23 17:41:56 +0200

    Programming opdracht

---
+++ resource management programming opdracht/Assets/Script/ColliderSphere.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderSphere : MonoBehaviour
+{
+    public int searchTries;
+    public float sizeIncrease;
+    public Scout scout;
+    public LayerMask stone, wood;
+
+    public void SearchforObject(LayerMask mask)
+    {
+        float currentsize = sizeIncrease;
+        bool found = false;
+        for (int i = 0; i < searchTries; i++)
+        {
+            // een for loop die ervoor zorgt dat hij steeds groter word totdat hij iets heeft gevonden en dan breakt hij de forloop
+            if (Physics.CheckSphere(transform.position, currentsize, mask))
+            {
+                found = true;
+                break;
+            }
+            //checkt hier of er iets in de radius is, zoja dan geeft hij aan dat hij iets heeft gevonden
+            currentsize += sizeIncrease;
+            //increased hiermee de size totdat hij iets heeft gevonden
+        }
+        if (found)
+        {
+
+            scout.target = Physics.OverlapSphere(transform.position, currentsize, mask)[0].transform;
+            scout.Exploring();
+            //wanneer hij iets heeft gevonden zorgt hij ervoor dat hij de locatie van de dichstbijzijnde target weet
+        }
+    }
+    public void SearchStone()
+    {
+        SearchforObject(stone);
+        // hij pakt de void SearchForObject en gaat daarin met de mask steen om de actie voor alleen steen te activeren en niet voor wood
+    }
+    public void SearchWood()
+    {
+        SearchforObject(wood);
+    }
+// hij pakt de void SearchForObject en gaat daarin met de mask wood om de actie voor alleen wood te activeren en niet voor steen
+}
---
+++ resource management programming opdracht/Assets/Script/Scout.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class Scout : MonoBehaviour {
+    public enum Actions { Idle, Walk, Action}
+    public Actions action;
+    public NavMeshAgent agent;
+    public Transform target;
+    public Transform home;
+    public int stoneAmount;
+    public int woodAmount;
+    public Text stoneText;
+    public Text woodText;
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+    }    // Update is called once per frame
+    void Update()
+    {
+        stoneText.text = stoneAmount.ToString();
+        woodText.text = woodAmount.ToString();
+        //hier zorgt hij ervoor dat de int hetzelfde word als de text zodat hij de ingame ui kan veranderen
+    }
+    public void Exploring()
+    {
+        agent.SetDestination(target.position);
+        action = Actions.Walk;
+        // hier krijgt de player een position waar hij heen moet lopen en als hij die heeft veranderd de enum naar Walk
+    }
+    public void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.tag == "stone"|| c.gameObject.tag == "wood")
+        {
+            Destroy(c);
+        }
+        //hij kijkt hier of hij collision heeft met iets wat stone of wood heet en dan gaat hij daaruit verder naar de Destroy void
+    }
+    public void Destroy(Collision c)
+    {
+        if (c.gameObject.tag == "stone")
+        {
+            stoneAmount += 1;
+        }
+        else if (c.gameObject.tag == "wood")
+        {
+            woodAmount += 1;
+        }
+        StartCoroutine(WaitAttack());
+        Destroy(c.gameObject);
+        agent.SetDestination(home.position);
+        StartCoroutine(IdleCheck());
+        // Binnen de collision checkt hij of het object van stone of van wood is gemaakt en als hij dat heeft gecheckt, destroyed hij het object en doet hij de score van die type omhoog
+    }
+    IEnumerator WaitAttack()
+    {
+        action = Actions.Action;
+        yield return new WaitForSeconds(3);
+        action = Actions.Walk;
+        //Hij zorgt er hier voor dat hij vanaf Action (wanneer hij de base attacked) naar Walk gaat.
+    }
+    IEnumerator IdleCheck()
+    {
+        while (Vector3.Distance(transform.position, home.transform.position) >= 5)
+        {
+            yield return null;
+        }
+        action = Actions.Idle;
+        // Hij checkt hier of hij in de idle state zit door de positie van home te pakken en die te vergelijken met de player.
+    }
+}
